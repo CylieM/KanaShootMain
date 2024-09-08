d3369d20f7efe7f3213bf68526ef8585e2b5a87b
whaillord069 <151481177+whaillord069@users.noreply.github.com>
2024-09-09 00:57:03 +0800

    Update KanaShoot.js (additional 46 hiragana and katakana char)

--- KanaShoot.js
+++ KanaShoot.js
@@ -5,11 +5,56 @@ const WAVE_DATA = [
         { kana: "う", romaji: "u" },
         { kana: "え", romaji: "e" },
         { kana: "お", romaji: "o" },
+        
         { kana: "か", romaji: "ka" },
         { kana: "き", romaji: "ki" },
         { kana: "く", romaji: "ku" },
         { kana: "け", romaji: "ke" },
         { kana: "こ", romaji: "ko" },
+
+        { kana: "は", romaji: "ha" },
+        { kana: "ひ", romaji: "hi" },
+        { kana: "ふ", romaji: "hu" },
+        { kana: "へ", romaji: "he" },
+        { kana: "ほ", romaji: "ho" },
+        
+        { kana: "ま", romaji: "ma" },
+        { kana: "み", romaji: "mi" },
+        { kana: "む", romaji: "mu" },
+        { kana: "め", romaji: "me" },
+        { kana: "も", romaji: "mo" },
+        
+        { kana: "な", romaji: "na" },
+        { kana: "に", romaji: "ni" },
+        { kana: "ぬ", romaji: "nu" },
+        { kana: "ね", romaji: "ne" },
+        { kana: "の", romaji: "no" },
+        
+        { kana: "ら", romaji: "ra" },
+        { kana: "り", romaji: "ri" },
+        { kana: "る", romaji: "ru" },
+        { kana: "れ", romaji: "re" },
+        { kana: "ろ", romaji: "ro" },
+        
+        { kana: "さ", romaji: "sa" },
+        { kana: "し", romaji: "shi" },
+        { kana: "す", romaji: "su" },
+        { kana: "せ", romaji: "se" },
+        { kana: "そ", romaji: "so" },
+        
+        { kana: "た", romaji: "ta" },
+        { kana: "ち", romaji: "chi" },
+        { kana: "つ", romaji: "tsu" },
+        { kana: "て", romaji: "te" },
+        { kana: "と", romaji: "to" },
+        
+        { kana: "や", romaji: "ya" },
+        { kana: "ゆ", romaji: "yu" },
+        { kana: "よ", romaji: "yo" },
+        { kana: "わ", romaji: "wa" },
+        { kana: "を", romaji: "wo" },
+        
+        { kana: "ん", romaji: "n" },
         // ... add more hiragana characters here ...
     ],
     [
@@ -18,11 +63,56 @@ const WAVE_DATA = [
         { kana: "ウ", romaji: "u" },
         { kana: "エ", romaji: "e" },
         { kana: "オ", romaji: "o" },
+        
         { kana: "カ", romaji: "ka" },
         { kana: "キ", romaji: "ki" },
         { kana: "ク", romaji: "ku" },
         { kana: "ケ", romaji: "ke" },
         { kana: "コ", romaji: "ko" },
+
+        { kana: "サ", romaji: "sa" },
+        { kana: "シ", romaji: "shi" },
+        { kana: "ス", romaji: "su" },
+        { kana: "セ", romaji: "se" },
+        { kana: "ソ", romaji: "so" },
+
+        { kana: "タ", romaji: "ta" },
+        { kana: "チ", romaji: "chi" },
+        { kana: "ツ", romaji: "tsu" },
+        { kana: "テ", romaji: "te" },
+        { kana: "ト", romaji: "to" },
+
+        { kana: "ナ", romaji: "na" },
+        { kana: "ニ", romaji: "ni" },
+        { kana: "ヌ", romaji: "nu" },
+        { kana: "ネ", romaji: "ne" },
+        { kana: "ノ", romaji: "no" },
+
+        { kana: "ハ", romaji: "ha" },
+        { kana: "ヒ", romaji: "hi" },
+        { kana: "フ", romaji: "fu" },
+        { kana: "ヘ", romaji: "he" },
+        { kana: "ホ", romaji: "ho" },
+
+        { kana: "マ", romaji: "ma" },
+        { kana: "ミ", romaji: "mi" },
+        { kana: "ム", romaji: "mu" },
+        { kana: "メ", romaji: "me" },
+        { kana: "モ", romaji: "mo" },
+
+        { kana: "ヤ", romaji: "ya" },
+        { kana: "ユ", romaji: "yu" },
+        { kana: "ヨ", romaji: "yo" },
+        { kana: "ワ", romaji: "wa" },
+        { kana: "ヲ", romaji: "wo" },
+
+        { kana: "ラ", romaji: "ra" },
+        { kana: "リ", romaji: "ri" },
+        { kana: "ル", romaji: "ru" },
+        { kana: "レ", romaji: "re" },
+        { kana: "ロ", romaji: "ro" },
+
+        { kana: "ン", romaji: "n" },
         // ... add more katakana characters here ...
     ],
     [
